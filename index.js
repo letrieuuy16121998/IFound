c4aae7d6676ba7bceade9638ea27e2b183adc352
VoThanhTai19 <93961593+VoThanhTai19@users.noreply.github.com>
2022-10-01 11:27:00 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
 
 app.get('/tin-tuc', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +10,11 @@ app.get('/tin-tuc', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+mongoose.connect('mongodb+srv://admin:tai1892001@cluster0.wv2mc.mongodb.net/IFound_Application?retryWrites=true&w=majority', {  
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('>>>>>>>>>> DB Connected!!!!!!'))
+.catch(err => console.log('>>>>>>>>> DB Error: ', err));
