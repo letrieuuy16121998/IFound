06e1e67400ab56ada9693280afea9889590ce459
VoThanhTai19 <93961593+VoThanhTai19@users.noreply.github.com>
2022-10-12 18:29:44 +0700

    update

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
+const path = require('path');
+const logger = require('morgan');
 
-app.get('/tin-tuc', (req, res) => {
-  res.send('Hello World!')
-})
+// khai báo các router
+const loginRouter = require('./routes/login')
+const productRouter = require('./routes/product')
+const categoryRouter = require('./routes/category')
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+//import các router
+app.use('/', loginRouter);
+app.use('/products', productRouter);
+app.use('/categories', categoryRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
